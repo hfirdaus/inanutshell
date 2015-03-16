797c7ba19dc5fe93d303e91d1e203fbbd22e9b5d
Hadiya F <aeondesires@hotmail.com>
2015-03-15 18:36:30 -0600

    Got rid of inexplicable back button, search display works (no functionality yet), and some other random debugging

--- app/src/main/java/com/example/hushcoolcat/inanutshell/MainActivity.java
+++ app/src/main/java/com/example/hushcoolcat/inanutshell/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.hushcoolcat.inanutshell;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.content.Intent;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -23,6 +21,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
--- app/src/main/java/com/example/hushcoolcat/inanutshell/search.java
+++ app/src/main/java/com/example/hushcoolcat/inanutshell/search.java
@@ -1,20 +1,18 @@
 package com.example.hushcoolcat.inanutshell;
 
-import android.app.SearchManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.SearchView;
-
+import android.app.SearchManager;
+import android.support.v7.widget.SearchView;
 
 public class search extends ActionBarActivity {
 
-    DatabaseTable db = new DatabaseTable(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,38 +20,25 @@ public class search extends ActionBarActivity {
         handleIntent(getIntent());
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater inflater = getMenuInflater();
         getMenuInflater().inflate(R.menu.menu_search, menu);
-
-        /*
-        public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.search_menu, menu);
-
-    SearchManager searchManager = (SearchManager)
-                            getSystemService(Context.SEARCH_SERVICE);
-    searchMenuItem = menu.findItem(R.id.search);
-    searchView = (SearchView) searchMenuItem.getActionView();
-
-    searchView.setSearchableInfo(searchManager.
-                            getSearchableInfo(getComponentName()));
-    searchView.setSubmitButtonEnabled(true);
-    searchView.setOnQueryTextListener(this);
-
-    return true;
-
-        */
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.search));
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(getApplicationContext(), search.class)));//setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
+    }
+
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+        }
     }
 
     @Override
@@ -70,20 +55,4 @@ public class search extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    protected void onNewIntent(Intent intent) {
-
-        handleIntent(intent);
-
-    }
-
-    private void handleIntent(Intent intent) {
-
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            Cursor c = db.getWordMatches(query, null);
-            //process Cursor and display results
-        }
-        }
-    }
-
+}
