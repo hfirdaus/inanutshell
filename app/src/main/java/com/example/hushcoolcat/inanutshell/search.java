9ad19981fc687f6dd6be83e1a1fb8f7f5719dd13
Hadiya F <aeondesires@hotmail.com>
2015-03-30 20:52:58 -0600

    various

--- app/src/main/java/com/example/hushcoolcat/inanutshell/search.java
+++ app/src/main/java/com/example/hushcoolcat/inanutshell/search.java
@@ -8,10 +8,14 @@ import android.database.Cursor;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.SearchManager;
 import android.support.v7.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class search extends ListActivity {
 
@@ -42,8 +46,29 @@ public class search extends ListActivity {
     private void handleIntent(Intent intent) {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            Cursor c = db.getWordMatches(query, null);
+            //Cursor c = db.getWordMatches(query, null);
+            findFile(query);
         }
+    }
+
+    private void findFile(String searchQuery) {
+        String [] queryWords = searchQuery.split(" ");
+        String [] fileNamesSaved = Context.fileList();
+        Log.d("First file", fileNamesSaved[0]);
+        /*
+        List<String> resultsArray = new ArrayList<String>();
+        ArrayList resultsArray = new String[0];
+        for (int i = 0; i < queryWords.length) {
+            for (int j = 0; j < fileNamesSaved.length) {
+                if (queryWords[i].equals(fileNamesSaved[j]))
+                    resultsArray.add(fileNamesSaved[j]);
+            }
+        }
+
+        for (int i = 0; i < resultsArray.size()) {
+            Log.d("Results", (String) resultsArray.get(i));
+        }
+        */
     }
 
     @Override
